d9ad3fb5630eb01638b8a0ad5f772f5148d9fb27
Felipe Buhrer Salles Rosa <66970402+Zoiodois@users.noreply.github.com>
2024-05-28 15:05:31 -0300

    Ignore

---
+++ envKeys.h
@@ -0,0 +1,13 @@
+//Wifi com 2 redes
+//const char *ssid[] = { "SSID1", "SSID2" };
+const char *ssid[] = { "Change" };
+const char *password = "**";
+const int numNetworks = sizeof(ssid) / sizeof(ssid[0]);
+
+
+//GSheets
+#define PROJECT_ID "**"
+// Service Account's client email
+#define CLIENT_EMAIL "**"
+// Service Account's private key
+const char PRIVATE_KEY[] PROGMEM = "**";
